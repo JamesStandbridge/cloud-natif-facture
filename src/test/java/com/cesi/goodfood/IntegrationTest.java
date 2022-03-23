575f33266ed7737c51738919b958a66f38f0d3c9
jamesstandbridge <james.standbridge.git@gmail.com>
2022-03-23 14:11:34 +0100

    Initial version of facture generated by generator-jhipster@7.5.0

---
+++ src/test/java/com/cesi/goodfood/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.cesi.goodfood;
+
+import com.cesi.goodfood.FactureApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = FactureApp.class)
+public @interface IntegrationTest {
+}
